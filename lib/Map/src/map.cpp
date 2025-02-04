971b38994813d3ad2ac05bf117efff0d03814a02
Guilherme <godoy.guilherme0@icloud.com>
2025-02-04 18:42:04 -0300

    Arduino Project Setup with PlataformIO IDE

---
+++ lib/Map/src/map.cpp
@@ -0,0 +1,18 @@
+#include "map.h"
+
+/*
+ * ====================
+ *  Points Definitions
+ * ====================
+*/
+
+Point::Point(int x, int y, int ocp)
+  : p{x, y, ocp} {};
+
+/*
+ * =================
+ *  Map Definitions
+ * =================
+*/
+
+
---
+++ lib/Map/src/map.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <Arduino.h>
+
+
+
+class Point {
+
+    /*
+     * A point is represented by a STRUCT with three components:
+     *  - x, which represents the x coordinate
+     *  - y, which represents the y coordinate
+     *  - ocp, which represents the occupancy (1: occupied, 0: free)
+    */
+
+    struct point {
+        int x;
+        int y;
+        int ocp;
+      } p;
+
+      public:
+
+      Point(int x, int y, int ocp);
+};
+
+class Map {
+
+    /*
+     * A Map is a collection of Point Objects distributed in a 2D array:
+     *
+     * For example:
+     *  - Map m1 = [[p1, p2], [p3, p4]];
+     *
+    */
+
+    public:
+
+    /*
+     * A Map constructor will receive an START and END point.
+     *
+     * The constructor will generate through loops the Map (which consists of an 2D array)
+    */
+
+    Map(Point start, Point end);
+};
+
