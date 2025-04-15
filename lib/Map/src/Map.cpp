66602add310c4c2712932e724b987ba8b5b4fa99
Guilherme <godoy.guilherme0@icloud.com>
2025-04-14 21:20:29 -0300

    Map Class Contructor Implementation

---
+++ lib/Map/src/Map.cpp
@@ -0,0 +1,22 @@
+#include "Map.h"
+#include "utility/SdFat.h"
+
+Map::Map(char *filename) : filename(filename) {
+
+  File f = SD.open(filename, O_RDWR);
+
+  while (f.available()) {
+
+    char c = f.read();
+
+    if (c == ',' && nOfRows < 1) {
+      nOfClms++;
+    }
+
+    if (c == '\n') {
+      nOfRows++;
+    }
+  }
+
+  nOfClms += 1; // Row has 2 ',' + 1 column
+}
