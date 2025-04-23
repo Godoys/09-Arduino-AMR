0d2c47dd6c67f63358eb161fd1688e901065f06c
Guilherme <godoy.guilherme0@icloud.com>
2025-04-22 22:02:31 -0300

    feat: Point pointStruct variables's Getters and Setters Implementation

--- lib/Map/src/Point.cpp
+++ lib/Map/src/Point.cpp
@@ -1,3 +1,15 @@
 #include "../include/Map.h"
 
 Point::Point(int x, int y, int ocp) : p{x, y, ocp} {};
+
+int Point::getX() { return p.x; }
+
+void Point::setX(int n) { p.x = n; }
+
+int Point::getY() { return p.y; }
+
+void Point::setY(int n) { p.y = n; }
+
+int Point::getOcp() { return p.ocp; }
+
+void Point::setOcp(int n) { p.ocp = n; }
