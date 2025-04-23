22df2f0ae55cfee5b0f6ab1ddb7e7644797c41d3
Guilherme <godoy.guilherme0@icloud.com>
2025-04-22 21:51:05 -0300

    feat: Point Class Instance Variables

---
+++ lib/Map/include/Map.h
@@ -0,0 +1,17 @@
+
+//------------------------------------------
+// Point Class Definition
+//------------------------------------------
+
+class Point {
+
+  // A Point is a reference to the (X, Y, OCP) values stored in a Map file.
+
+  struct pointStruct {
+    int x;
+    int y;
+    int ocp;
+  };
+
+  pointStruct p;
+};
