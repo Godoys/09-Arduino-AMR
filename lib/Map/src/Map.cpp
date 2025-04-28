c72845abb111fadf4998d0393cb9c4d67c3e4bdf
Guilherme <godoy.guilherme0@icloud.com>
2025-04-28 19:19:09 -0300

    feat: Map Constructor Implementation

--- lib/Map/src/Map.cpp
+++ lib/Map/src/Map.cpp
@@ -4,7 +4,46 @@
 //           Map Public Methods
 //------------------------------------------
 
-Map::Map(char filename) : filename(filename) {}
+Map::Map(char *filename) : filename(filename) {
+
+  File f = SD.open(filename, O_RDWR);
+
+  // Buffer to contain line characters.
+
+  char *buffer;
+
+  buffer = readCharsUntil('\n', f);
+
+  // Counts the number of ROWS and the number of COLUMNS in the file.
+
+  for (int i = 0; i < 1024; i++) {
+
+    char c = buffer[i];
+
+    // The number of ',' plus one in a line means the number of columns it has.
+
+    if (c == ',') {
+      nOfClms++;
+    }
+  }
+
+  nOfClms++;
+
+  // Return to first character in file.
+
+  f.seek(0);
+
+  while (f.available()) {
+
+    char c = f.read();
+
+    // The number of '\n' in a file means the number of lines it has.
+
+    if (c == '\n') {
+      nOfRows++;
+    }
+  }
+}
 
 //------------------------------------------
 //           Map Private Methods
