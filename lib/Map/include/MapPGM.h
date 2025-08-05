17d746a7c18f1ea0497d55c2329172e8331a55f8
Godoys <godoy.guilherme0@icloud.com>
2025-08-04 21:16:43 -0300

    feat: Added instance variables

--- lib/Map/include/MapPGM.h
+++ lib/Map/include/MapPGM.h
@@ -5,6 +5,11 @@ class MapPGM {
     // A Map is an interface to a Portable Grey Map (PGM) file. It defines commands such as
     // create, delete, read, write, etc.
 
+    string fileName;
+    int nOfRows;
+    int nOfClms;
+    int maxVal;
+
 public:
 
     MapPGM(string fileName, int nOfRows, int nOfClms, int maxVal = 1);
