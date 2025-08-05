f34e8ee09ca84ae76ed6e03940173c76fc8e2c05
Godoys <godoy.guilherme0@icloud.com>
2025-08-04 21:07:15 -0300

    feat: Added MapPGM file and MapPGM constructor declaration

---
+++ lib/Map/include/MapPGM.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class MapPGM {
+
+    // A Map is an interface to a Portable Grey Map (PGM) file. It defines commands such as
+    // create, delete, read, write, etc.
+
+public:
+
+    MapPGM(string fileName, int nOfRows, int nOfClms, int maxVal = 1);
+};
