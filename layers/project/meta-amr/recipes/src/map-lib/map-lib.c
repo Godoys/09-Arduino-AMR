f716784b0b22687dcb09ccbc2deaf49c2262ae90
Godoys <godoy.guilherme0@icloud.com>
2025-11-14 03:47:57 +0000

    feat: Implemented create_map function

--- layers/project/meta-amr/recipes/src/map-lib/map-lib.c
+++ layers/project/meta-amr/recipes/src/map-lib/map-lib.c
@@ -28,4 +28,28 @@ FILE *open_map(char *file_name, char *mode) {
 
 void close_map(FILE *file) { fclose(file); }
 
+// create_map assumes that the map starts at (0,0) and ends at
+// (end_coordinate,end_coordinate).
+
+FILE *create_map(char *map_name, int end_coordinate) {
+  // Creates new map file with map_name.
+  char *file_name;
+
+  if (strstr(map_name, ".map") == NULL) {
+    file_name = strcat(map_name, ".map");
+  } else {
+    file_name = map_name;
+  }
+
+  FILE *map_ptr = open_map(file_name, "w");
+
+  for (int i = 0; i < end_coordinate; i++) {
+    fprintf(map_ptr, "%d,%d\n", i, i);
+  }
+
+  close_map(map_ptr);
+
+  return map_ptr;
+}
+
 int main() {}
