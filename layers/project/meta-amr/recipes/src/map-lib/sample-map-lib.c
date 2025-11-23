3e69d4fe7d55a0fde422323223ff2112fe62241b
Godoys <godoy.guilherme0@icloud.com>
2025-11-23 19:26:57 +0000

    feat: Implemented map_read function

--- layers/project/meta-amr/recipes/src/map-lib/sample-map-lib.c
+++ layers/project/meta-amr/recipes/src/map-lib/sample-map-lib.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,3 +17,24 @@ struct point {
   int x; /* X coordinate of the point */
   int y; /* Y coordinate of the point */
 };
+
+ssize_t map_read(void *c, char *buf, size_t size) {
+  ssize_t nbytes;
+  struct map_cookie *cookie = c;
+
+  /* Fetch minimum of bytes requested and bytes availabe */
+
+  nbytes = size;
+
+  if (cookie->offset + size > cookie->endpos) {
+    nbytes = cookie->endpos - cookie->offset;
+  }
+  if (nbytes < 0) {
+    nbytes = 0; /* offset may be past endpos */
+  }
+
+  memcpy(buf, cookie->buf + cookie->offset, nbytes);
+
+  cookie->offset += nbytes;
+  return nbytes;
+}
