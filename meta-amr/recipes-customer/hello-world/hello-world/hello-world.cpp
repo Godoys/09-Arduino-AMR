df54b7aebaa8bb8cc5bbf2436522725bc39ffb64
Godoys <godoy.guilherme0@icloud.com>
2026-04-20 22:02:48 +0100

    fix: Did a improvided merge

--- meta-amr/recipes-customer/hello-world/hello-world/hello-world.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:55709af890ce55d65b4422246bd371c79abe7a8c1fbdee04721b4087183c1479
-size 110
--- meta-amr/recipes-customer/hello-world/hello-world_1.0.0.bb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4b08907b6bcfc047a47e6d8946d4222534a1719c6c39455c78be865da0ef8ab4
-size 732
