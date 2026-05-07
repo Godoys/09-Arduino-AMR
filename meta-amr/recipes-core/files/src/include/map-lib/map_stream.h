0cbd580a9b882396b04d07de2f6a4b01db1bf4a1
Godoys <godoy.guilherme0@icloud.com>
2026-05-07 21:08:21 +0100

    fix: Removed unecessary variable inside cookie struct

--- meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8394076c8a74a7d897d722abfa987b7ae47e022ec5ea8b58388675028a4d97
-size 641
+oid sha256:d720ae384a82e0f9128ab185cbbd689e076f3822d086af3cf0632db1a1629a3b
+size 626
