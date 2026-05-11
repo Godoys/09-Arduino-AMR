ca33a35f68d9e0e47569e1a8539458ca32cf48f9
Godoys <godoy.guilherme0@icloud.com>
2026-05-11 20:30:47 +0100

    feat: Changed main to test new but not yet developed map_write function

--- meta-amr/recipes-core/files/src/main.c
+++ meta-amr/recipes-core/files/src/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5064b444f3e3845ea75003475f2addc3ccdd01ea9eeeb562077cb8e3e84fdc84
-size 875
+oid sha256:07055b7da5d4dfe8324d7cb8a57692d2b0e54721ccec32f870bfbb1d5f1c4e8f
+size 306
