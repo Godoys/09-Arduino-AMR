a536f19b25569694b39f8108f848a378da4df19c
Godoys <godoy.guilherme0@icloud.com>
2026-05-11 21:25:07 +0100

    fix: Adde file-descriptor variable for map_cookie

--- meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d0d7a47c23a6c83a72ecfbb863ea9e04343efd6800b91ffe2314a9019080de6
-size 618
+oid sha256:e75d24e828f5d28a7dbc7bf183164747b6a66408146577e2c8aef435e2974728
+size 685
