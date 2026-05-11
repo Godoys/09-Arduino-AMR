e6a7cdb5ea2ead47af98610f62242b7b3ad15582
Godoys <godoy.guilherme0@icloud.com>
2026-05-11 20:39:19 +0100

    fix: Changed map_open declaration to include map_path

--- meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f3a7ccfb361282a34522a13d4f6a1603914ac55c90dbe7d9ebef2e781ed2d5a
-size 584
+oid sha256:4d0d7a47c23a6c83a72ecfbb863ea9e04343efd6800b91ffe2314a9019080de6
+size 618
