2612483aac038b38a87e6419e34d887334b37b61
Godoys <godoy.guilherme0@icloud.com>
2026-05-12 21:23:40 +0100

    feat: Added map_check_signature and update map_write_signature declaration

--- meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dcce7158b4866bd4de684351cf8755835dd04ea977b652ea4bb865e51b55eac
-size 666
+oid sha256:5606d1e534dd63798a6b6b65284868eb41209f73453835891a4947bcc918537e
+size 844
