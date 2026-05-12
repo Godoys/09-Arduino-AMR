e23204c79594ad8224766c74cc581bdc4184772a
Godoys <godoy.guilherme0@icloud.com>
2026-05-12 21:31:59 +0100

    fix: Removed map_path from map_write_signature declaration

--- meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map_stream.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5606d1e534dd63798a6b6b65284868eb41209f73453835891a4947bcc918537e
-size 844
+oid sha256:ad31a62c611afbb063de2efc44cba2430a92b9db95b807db3d2fdbe2098e1c9e
+size 828
