c11daa7a227e7e94f9a730e35396ab35970f0759
Godoys <godoy.guilherme0@icloud.com>
2026-05-20 16:32:12 +0100

    fix: Fixed missing semicolon and wrong datatype for point

--- meta-amr/recipes-core/files/src/include/map-lib/map.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map.h
@@ -6,39 +6,39 @@
  *
  * @param int x x-coordinate of the point.
  * @param int y y-coordinate of the point.
- * @param bool empty tells if point is an empty space.
+ * @param int empty 1 for not empty and 0 for empty.
 */
 
 struct point
 {
     int x;
     int y;
-    bool empty;
-}
+    int empty;
+};
 
 /* 
  * Populates the map file with point values from (0, 0)
  * to (p->x, p->y);
  *
  * @param void *c structure containing map reference.
- * @param point *p point that will be use as end of the map.
+ * @param void *p point that will be use as end of the map.
  * 
 */
 
-int map_populate(void *c, point *p);
+int map_populate(void *c, void *p);
 
 /*
  * Creates an obstacle in the map from the point START
  * until the point END.
  *
  * @param void *c structure containing map reference.
- * @param point *start beginning of obstacle.
- * @paran point *end end of obstacle.
+ * @param void *start beginning of obstacle.
+ * @paran void *end end of obstacle.
  *
  * @return 0 in success, -1 otherwise.
 */
 
-int map_obstacle(void *c, point *start, point *end);
+int map_obstacle(void *c, void *start, void *end);
 
 /*
  * Inflates the obstacle in the map by a given rate.
