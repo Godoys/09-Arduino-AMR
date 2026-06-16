db3448521ee5f7c832ae799432e7720449812c30
Godoys <godoy.guilherme0@icloud.com>
2026-06-16 18:06:06 -0300

    feat: Added tests for map_populate

--- meta-amr/recipes-core/files/src/main.c
+++ meta-amr/recipes-core/files/src/main.c
@@ -1,13 +1,20 @@
+#include "map.h"
 #include "map_stream.h"
 
 int main(int argc, char *argv[])
 {
-    FILE *stream = map_open("/home/godoy/Documents/01-Computer-Science/01-Projects/09-AMR/meta-amr/recipes-core/files/src/rsc/sample.map");
+    FILE *stream = map_open("/home/godoy/Documents/01-Adventures/09-AMR/meta-amr/recipes-core/files/src/rsc/sample.map");
 
     if (stream == NULL) {
         perror("[!] Error opening file");
         exit(EXIT_FAILURE);
     }
+
+    struct point *p;
+    p->x = 10;
+    p->y = 10;
+
+    map_populate(stream, p);
 
     fclose(stream);
 
