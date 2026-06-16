103e2e12884319957fd928c2dd3bfc1c01910aa8
Godoys <godoy.guilherme0@icloud.com>
2026-06-16 17:59:44 -0300

    fix: Changed all parameters that were using FILE type to Void

--- meta-amr/recipes-core/files/src/include/map-lib/map.h
+++ meta-amr/recipes-core/files/src/include/map-lib/map.h
@@ -20,44 +20,44 @@ struct point
  * Populates the map file with point values from (0, 0)
  * to (p->x, p->y);
  *
- * @param FILE *s stream containing map file.
+ * @param void *s stream containing map file.
  * @param void *p point that will be use as end of the map.
 */
 
-int map_populate(FILE *s, void *p);
+int map_populate(void *s, void *p);
 
 /*
  * Creates an obstacle in the map from the point START
  * until the point END.
  *
- * @param FILE *s stream containing map file.
+ * @param void *s stream containing map file.
  * @param void *start beginning of obstacle.
  * @paran void *end end of obstacle.
  *
  * @return 0 in success, -1 otherwise.
 */
 
-int map_obstacle(FILE *s, void *start, void *end);
+int map_obstacle(void *s, void *start, void *end);
 
 /*
  * Inflates the obstacle in the map by a given rate.
  *
- * @param FILE *s stream containing map file.
+ * @param void *s stream containing map file.
  * @param int rate rate to inflate obstacles.
  *
  * @return 0 in success, -1 otherwise.
 */
 
-int map_inflate(FILE *s, int rate);
+int map_inflate(void *s, int rate);
 
 /*
  * Creates obstacles around the perimeter of the map.
  *
- * @param FILE *s stream containing map file.
+ * @param void *s stream containing map file.
  *
  * @return 0 in success, -1 otherwise.
 */
 
-int map_enclose(FILE *s);
+int map_enclose(void *s);
 
 #endif
