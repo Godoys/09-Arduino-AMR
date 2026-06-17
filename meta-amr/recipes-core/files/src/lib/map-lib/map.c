d0aa73a2ca0226eb2054e649f72ad670fd254391
Godoys <godoy.guilherme0@icloud.com>
2026-06-16 22:51:53 -0300

    feat: Implemented map_populate

--- meta-amr/recipes-core/files/src/lib/map-lib/map.c
+++ meta-amr/recipes-core/files/src/lib/map-lib/map.c
@@ -1,11 +1,38 @@
 #include "map.h"
 #include "map_stream.h"
 
-int map_populate(FILE *s, void *p)
+int map_populate(void *s, void *p)
 {
-/*
- * create a array of points from (0,0) to *p
- *
- * use write function to write in FILE *s
-*/
+    FILE *stream = s;
+    struct point *end_point = p;
+
+    size_t map_size = end_point->x * end_point->y;
+
+    struct point *buf = malloc(sizeof(struct point) * map_size);
+
+    int points_counter = 0;
+
+    while (points_counter < map_size) {
+        for (int y = 0; y < end_point->y; y++) {
+            for (int x = 0; x < end_point->x; x++) {
+                struct point *new_point;
+                new_point->x = x;
+                new_point->y = y;
+                new_point->v = 0;
+
+                buf[points_counter] = *new_point;
+
+                points_counter += 1;
+            }
+        }
+    }
+
+    if (fwrite(buf, sizeof(struct point), map_size, stream) < 1){
+        perror("[!] Write");
+        exit(EXIT_FAILURE);
+    }
+
+    free(buf);
+
+    return 0;
 }
